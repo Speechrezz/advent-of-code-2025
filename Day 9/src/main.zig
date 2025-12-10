019068853d5c75e61a58be70409ce0112003cb33
Mark <mark@xynth.audio>
2025-12-09 18:48:57 -0800

    Cleaned up code a bit

--- Day 9/src/main.zig
+++ Day 9/src/main.zig
@@ -1,27 +1,19 @@
 const std = @import("std");
 const parsing = @import("parsing.zig");
 
-fn computeArea(point1: parsing.Point, point2: parsing.Point) u64 {
-    const distance_x = @abs(point1.x - point2.x) + 1;
-    const distance_y = @abs(point1.y - point2.y) + 1;
-
-    return distance_x * distance_y;
+fn part1(floor: *parsing.Floor) u64 {
+    return floor.area_list.items[0].size;
 }
 
-fn part1(point_list: parsing.PointList) u64 {
-    var largest: u64 = 0;
-
-    for (0..point_list.items.len) |i| {
-        for (i + 1..point_list.items.len) |j| {
-            const point1 = point_list.items[i];
-            const point2 = point_list.items[j];
-
-            const area = computeArea(point1, point2);
-            largest = @max(largest, area);
+fn part2(floor: *parsing.Floor) u64 {
+    for (floor.area_list.items) |area| {
+        if (floor.isAreaInside(area)) {
+            return area.size;
         }
     }
 
-    return largest;
+    std.debug.assert(false); // Should never reach here
+    return 0;
 }
 
 pub fn main() !void {
@@ -34,10 +26,11 @@ pub fn main() !void {
     defer allocator.free(contents);
 
     // Parse
-    var point_list = try parsing.parsePoints(allocator, contents);
-    defer point_list.deinit(allocator);
+    var floor: parsing.Floor = undefined;
+    try floor.init(allocator, contents);
+    defer floor.deinit(allocator);
 
     // Solve
-    std.debug.print("[Part 1] Solution={}\n", .{part1(point_list)});
-    //std.debug.print("[Part 2] Solution={}\n", .{part2(contents)});
+    std.debug.print("[Part 1] Solution={}\n", .{part1(&floor)});
+    std.debug.print("[Part 2] Solution={}\n", .{part2(&floor)});
 }
--- Day 9/src/parsing.zig
+++ Day 9/src/parsing.zig
@@ -10,6 +10,10 @@ pub fn readFile(allocator: std.mem.Allocator, file_path: []const u8) ![]u8 {
 pub const Point = struct {
     x: i64,
     y: i64,
+
+    pub fn eql(self: @This(), other: @This()) bool {
+        return self.x == other.x and self.y == other.y;
+    }
 };
 
 pub const PointList = std.ArrayList(Point);
@@ -31,3 +35,115 @@ pub fn parsePoints(allocator: std.mem.Allocator, contents: []const u8) !PointLis
 
     return point_list;
 }
+
+pub const Area = struct {
+    point1: Point,
+    point2: Point,
+    size: u64 = 0,
+
+    pub fn init(point1: Point, point2: Point) @This() {
+        const distance_x = @abs(point1.x - point2.x) + 1;
+        const distance_y = @abs(point1.y - point2.y) + 1;
+
+        return .{
+            .point1 = point1,
+            .point2 = point2,
+            .size = distance_x * distance_y,
+        };
+    }
+};
+
+pub const AreaList = std.ArrayList(Area);
+
+pub fn generateSortedAreaList(allocator: std.mem.Allocator, point_list: PointList) !AreaList {
+    var area_list: AreaList = .empty;
+
+    for (0..point_list.items.len) |i| {
+        for (i + 1..point_list.items.len) |j| {
+            try area_list.append(
+                allocator,
+                Area.init(
+                    point_list.items[i],
+                    point_list.items[j],
+                ),
+            );
+        }
+    }
+
+    std.sort.block(
+        Area,
+        area_list.items,
+        {},
+        comptime struct {
+            pub fn inner(_: void, a: Area, b: Area) bool {
+                return a.size > b.size;
+            }
+        }.inner,
+    );
+
+    return area_list;
+}
+
+pub const Floor = struct {
+    point_list: PointList,
+    area_list: AreaList,
+
+    pub fn init(self: *@This(), allocator: std.mem.Allocator, contents: []const u8) !void {
+        self.point_list = try parsePoints(allocator, contents);
+        self.area_list = try generateSortedAreaList(allocator, self.point_list);
+    }
+
+    pub fn deinit(self: *@This(), allocator: std.mem.Allocator) void {
+        self.point_list.deinit(allocator);
+        self.area_list.deinit(allocator);
+    }
+
+    fn getIncrement(start: i64, end: i64) i64 {
+        if (start == end) return 0;
+
+        const distance = end - start;
+        return @divTrunc(distance, (@as(i64, @intCast(@abs(distance)))));
+    }
+
+    fn isPointInsideRectangle(point: Point, top_left: Point, bottom_right: Point) bool {
+        const check_x = top_left.x < point.x and point.x < bottom_right.x;
+        const check_y = top_left.y < point.y and point.y < bottom_right.y;
+        return check_x and check_y;
+    }
+
+    pub fn isAreaInside(self: *const @This(), area: Area) bool {
+        const top_left: Point = .{
+            .x = @min(area.point1.x, area.point2.x),
+            .y = @min(area.point1.y, area.point2.y),
+        };
+
+        const bottom_right: Point = .{
+            .x = @max(area.point1.x, area.point2.x),
+            .y = @max(area.point1.y, area.point2.y),
+        };
+
+        // Travel along line formed by the two points.
+        // If any part of this line is inside the area, then the area has to be invalid.
+        var prev_point = self.point_list.getLast();
+        for (self.point_list.items) |current_point| {
+            const increment_x = getIncrement(prev_point.x, current_point.x);
+            const increment_y = getIncrement(prev_point.y, current_point.y);
+
+            var point = prev_point;
+            while (!point.eql(current_point)) {
+                defer {
+                    point.x += increment_x;
+                    point.y += increment_y;
+                }
+
+                if (isPointInsideRectangle(point, top_left, bottom_right)) {
+                    return false;
+                }
+            }
+
+            prev_point = current_point;
+        }
+
+        return true;
+    }
+};
